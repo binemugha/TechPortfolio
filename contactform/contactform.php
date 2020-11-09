85d28758538b9956ad62556bbe8188bfbb9dd2e2
Benjamin Inemugha <Bennyblasco@Benjamins-MacBook-Pro.local>
2020-11-09 11:53:45 +0100

    Contact form

---
+++ contactform/contactform.php
@@ -0,0 +1,28 @@
+<?php
+
+    if(isset($_POST['btn-send']))
+    {
+       $UserName = $_POST['name'];
+       $Email = $_POST['email'];
+       $Subject = $_POST['subject'];
+       $Msg = $_POST['message'];
+
+       if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
+       {
+           header('location:index.php?error');
+       }
+       else
+       {
+           $to = "bennyblasco@gmail.com";
+
+           if(mail($to,$Subject,$Msg,$Email))
+           {
+               header("location:index.html?success");
+           }
+       }
+    }
+    else
+    {
+        header("location:index.html");
+    }
+?>
